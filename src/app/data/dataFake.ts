5859d9e060da291b4e5e0939d60f6488178ff2c7
Elvin Conopoy <elvin.conopoy@gmail.com>
2023-09-13 21:31:29 -0300

    Add pages para visualizar conteudo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,52 @@
+export const dataFake = [
+	{
+		"id":"1",
+		"photo":"https://images3.alphacoders.com/228/228065.jpg",
+		"title":"Pirera noticia",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"2",
+		"photo":"https://images4.alphacoders.com/228/thumbbig-228056.webp",
+		"title":"Pirera noticia 2",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"3",
+		"photo":"https://c4.wallpaperflare.com/wallpaper/931/11/696/dragon-ball-super-saiyan-blue-dragon-ball-super-vegeta-wallpaper-preview.jpg",
+		"title":"Pirera noticia 3",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"4",
+		"photo":"https://images4.alphacoders.com/686/thumbbig-68646.webp",
+		"title":"Pirera noticia 4",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"5",
+		"photo":"https://images2.alphacoders.com/194/thumbbig-194190.webp",
+		"title":"Pirera noticia 4",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"6",
+		"photo":"https://images4.alphacoders.com/700/700487.png",
+		"title":"Pirera noticia 4",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"7",
+		"photo":"https://images6.alphacoders.com/750/thumbbig-750318.webp",
+		"title":"Pirera noticia 4",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	},
+	{
+		"id":"8",
+		"photo":"https://images7.alphacoders.com/769/thumbbig-769813.webp",
+		"title":"Pirera noticia 4",
+		"descriptiom":"bla bla bla, bla bla bla...."
+	}
+]
+
+
